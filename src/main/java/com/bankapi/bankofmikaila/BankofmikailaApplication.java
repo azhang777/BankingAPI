6976bbbea721c9225b32169eb363dd863917d863
Andy Zhang <andyzhanggm@gmail.com>
2023-11-15 09:46:37 -0500

    Set up for remote repo

---
+++ src/main/java/com/bankapi/bankofmikaila/BankofmikailaApplication.java
@@ -0,0 +1,13 @@
+package com.bankapi.bankofmikaila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankofmikailaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankofmikailaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankapi/bankofmikaila/BankofmikailaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankapi.bankofmikaila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankofmikailaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
