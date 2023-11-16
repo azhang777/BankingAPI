350ba2facb19cc211b85b380914773c556a74dcf
alejandrabedolla <blancabedolla4@gmail.com>
2023-11-16 12:23:57 -0500

    deleted address repo

--- src/main/java/com/bankapi/bankofmikaila/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bankapi.bankofmikaila.repository;
-
-import com.bankapi.bankofmikaila.model.Address;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends CrudRepository<Address, Long> {
-}
