41b4740bfd6b50a55b37ae241a21d6175de81607
alejandrabedolla <blancabedolla4@gmail.com>
2023-11-25 21:44:51 -0500

    worked on billresponse and billservice

--- src/main/java/com/bankapi/bankofmikaila/response/BillResponse.java
+++ src/main/java/com/bankapi/bankofmikaila/response/BillResponse.java
@@ -1,12 +1,7 @@
 package com.bankapi.bankofmikaila.response;
 
-
-
+import com.bankapi.bankofmikaila.dto.Detail;
 import org.springframework.stereotype.Component;
-
-
-   
-
 import com.bankapi.bankofmikaila.model.Account;
 import com.bankapi.bankofmikaila.model.Bill;
 import com.bankapi.bankofmikaila.repository.BillRepository;
@@ -17,16 +12,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
  /**
-     * @TODO - Autowire the bill service
-     *
-     * @Autowired
-     * private BillService billService;
      *
      * @Todo - create 6 methods that all return a response entity for each method in the bill service
      *
@@ -90,47 +80,57 @@ import java.util.stream.Collectors;
 @Component
 public class BillResponse {
 
-    @Autowired
-    private BillService billService;
+     @Autowired
+     private BillService billService;
 
-    public List<Bill> getAllBillsForAccount(String accountId) {
-        List<Bill> bills = billService.getAllBills();
-        return bills;
-    }
+     public ResponseEntity<?> getAllBills(Long accountId) {
+         List<Bill> bills = billService.getAllBills(accountId);
+         Detail detail = new Detail();
+         detail.setData(bills);
+         detail.setCode(HttpStatus.OK.value());
+         return new ResponseEntity<>(detail, HttpStatus.OK);
+     }
 
-    public Bill getBillById(Long billId) {
-        Optional<Bill> bill = billService.getBillById(billId);
-        return bill.orElse(null);
-    }
 
-    public List<Bill> getAllBillsForCustomer(String customerId) {
-        List<Bill> bills = billService.getAllBills();
-        return bills;
-    }
+     public ResponseEntity<?> getBillById(Long billId) {
+         Optional<Bill> bill = billService.getBillById(billId);
+         Detail detail = new Detail();
+         if (bill.isPresent()) {
+             detail.setData(bill.get());
+             detail.setCode(HttpStatus.OK.value());
+             return new ResponseEntity<>(detail, HttpStatus.OK);
+         } else {
+             detail.setMessage("Bill not found");
+             return new ResponseEntity<>(detail, HttpStatus.NOT_FOUND);
+         }
+     }
 
-    public Bill createBillForAccount(Account accountId, Bill bill) {
-        Bill createdBill = billService.createBillForAccount(accountId, bill);
-        return createdBill;
-    }
+     public ResponseEntity<?> getBillsByCID(Long customerId) {
+         List<Bill> bills = billService.getBillsByCID(customerId);
+         Detail detail = new Detail();
+         detail.setData(bills);
+         detail.setCode(HttpStatus.OK.value());
+         return new ResponseEntity<>(detail, HttpStatus.OK);
+     }
 
-    public ResponseEntity<Bill> updateBill(Long billId, Bill updatedBill) {
-        Optional<Bill> existingBill = billService.updateBill(billId, updatedBill);
+     public ResponseEntity<?> createBill(Bill bill, Long accountId) {
+         Detail detail = new Detail();
+         Bill createdBill = billService.createBillForAccount(accountId, bill);
+         detail.setData(createdBill);
+         detail.setCode(HttpStatus.OK.value());
+         detail.setMessage("Created bill and added it to the account");
+         return new ResponseEntity<>(detail, HttpStatus.OK);
+     }
 
-        if (existingBill.isPresent()) {
-            Bill savedBill = existingBill.get();
-            return new ResponseEntity<>(savedBill, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
+     public ResponseEntity<?> updateBill(Long billId, Bill bill ){
+     Detail detail = new Detail();
+     detail.setMessage("Accepted bill modification");
+     return new ResponseEntity<>(detail, HttpStatus.ACCEPTED);
+     }
 
-    public ResponseEntity<String> deleteBill(Long billId) {
-        boolean deleted = billService.deleteBill(billId);
-        if (deleted) {
-            return new ResponseEntity<>("Bill deleted successfully", HttpStatus.NO_CONTENT);
-        } else {
-            return new ResponseEntity<>("Bill not found", HttpStatus.NOT_FOUND);
-        }
-    }
-}
+
+     public ResponseEntity<?> deleteBill(Long billId) {
+         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+     }
+ }
 
--- src/main/java/com/bankapi/bankofmikaila/service/BillService.java
+++ src/main/java/com/bankapi/bankofmikaila/service/BillService.java
@@ -1,220 +1,125 @@
 package com.bankapi.bankofmikaila.service;
 
+import com.bankapi.bankofmikaila.exception.BillByIdNotFound;
+import com.bankapi.bankofmikaila.exception.BillsByAccountIdNotFoundException;
+import com.bankapi.bankofmikaila.exception.BillsByCustomerIdNotFoundException;
 import com.bankapi.bankofmikaila.model.Account;
+
 import com.bankapi.bankofmikaila.model.Bill;
+import com.bankapi.bankofmikaila.repository.AccountRepository;
 import com.bankapi.bankofmikaila.repository.BillRepository;
+import com.bankapi.bankofmikaila.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.sym.error;
 
 @Service
 public class BillService {
-    /**
-     * @REVIEW - Love how you thinking with the bill repository and making the map
-     * with id for key and the bill object for the value but there is an easier way
-     *
-     * @TODO - Auto wire your bill repository and the account repository looks like this
-     *
-     * @Autowired
-     * private BillRepository billRepository
-     *
-     * @Autowired
-     * private AccountRepository accountRepository;
-     *
-     * @Autowired
-     * private CustomerRepository customerRepository;
-     *
-     *
-     *
-     * @TODO - must add 2 queries in the BillRepository
-     *
-     *
-     * @TODO - next we to change the logic in all of the methods and add 1 more method
-     *
-     *
-     * @TODO - we can focus the exception handlers next.
-     */
-
-
-
-    /**
-     * @Delete  - line 42
-     */
-    private static final Map<Long, Bill> billRepository = new HashMap<>();
-
-    /**
-     * @Delete - line 47
-     */
     private static long nextBillId;
 
-    @Autowired
-    private BillRepository billRepository;
+@Autowired
+private BillRepository billRepository;
 
-
-    /**
-     * @Change
-     *
-     * @Todo - must get all bills by account Id. Logic is similar to other services.
-     *
-     *
-     *  public List<Bill> getAllBills(Long accountId) {
-     *
-     *          if(accountRepository.findById(accountId).isEmpty(){
-     *              throw new BillsByAccountIdNotFound(“error fetching bills”)
-     *          }
-     *
-     *         return billRepository.findBillsByAccountId(accountId)
-     *     }
-     *
-     */
-    public List<Bill> getAllBills() {
+ @Autowired
+ private AccountRepository accountRepository;
+ @Autowired
+ private CustomerRepository customerRepository;
 
 
 
-        return new ArrayList<>(billRepository.values());
+    public List<Bill> getAllBills(Long accountId) {
+        Optional<Account> account = accountRepository.findById(accountId);
+        if (account.isPresent()) {
+            Set<Bill> billsSet = billRepository.findBillsByAccountId(accountId);
+            // Convert Set to List
+            List<Bill> billsList = new ArrayList<>(billsSet);
+            return billsList;
+        } else {
+            throw new BillsByAccountIdNotFoundException("Error fetching bills");
+        }
     }
 
-    /**
-     *
-     * @TODO change logic
-     *
-     *  public Bill getBillById(Long id) {
-     *  var bill = billRepository.findById(id);
-     *      if(bill.isEmpty()){
-     *          throw new BillByIdNotFound(“error fetching bill with id: ” + id);
-     *      }
-     *
-     *  return   bill.get();
-     *     }
-     * @return
-     */
+
     public Bill getBillById(Long id) {
-        return billRepository.get(id);
-
-        return (List<Bill>) billRepository.findAll();
-    }
-
-    public Optional<Bill> getBillById(Long id) {
-        return billRepository.findById(id);
+        var bill = billRepository.findById(id);
+        if (bill.isEmpty()){
+            throw new BillsByAccountIdNotFoundException("error fetching bill with id + " id);
+        }
+        return   bill.get();
 
     }
 
-    /**
-     * @TODO - need another method to get all bills associated with the given customerId
-     *
-     * public Bill getBillsByCID(Long customerId){
-     * var customer = customerRepository.findById(customerId);
-     * if(customer.isEmpty()){
-     *     throw new BillsByCustomerIdNotFound("error fetching bills");
-     * }
-     *    return billRepository.findBillsByCustomertId(customerId);
-     * }
-     */
 
-    /**
-     *
-     * @TODO - we must be able to create a bill by an accountId
-     *         Looks like this.
-     *
-     *     public Bill createBill(Bill bill, Long accountId){
-     *         Optional<Account> account = accountRepository.findById(accountId);
-     *
-     *         if( account.isEmpty()){
-     *         throw new BillsByAccountIdNotFound("Error creating bill: Account not found");
-     *         }
-     *         bill.setAccount(account.get());
-     *     return billRepository.save(bill);
-     *     }
-     */
+
+    public Set <Bill> getBillsByCID(Long customerId){
+     var customer = customerRepository.findById(customerId);
+     if(customer.isEmpty()){
+    throw new BillsByCustomerIdNotFoundException("error fetching bills");
+    }
+    return billRepository.findBillsByCustomertId(customerId);
+    }
+
+
+       public Bill createBill(Bill bill, Long accountId){
+        Optional<Account> account = accountRepository.findById(accountId);
+           {
+
+      if( account.isEmpty()){
+     throw new BillsByAccountIdNotFoundException("Error creating bill: Account not found");
+     }
+      bill.setAccount(account.get());
+     return billRepository.save(bill);
+}
+
+
     public Bill createBill(Bill bill) {
 
         return billRepository.save(bill);
     }
 
 
-    /**
-     * @Todo - updateBill should look like this inside
-     *
-     * Optional<Bill> existingBill = billRepository.findById(id);
-     *
-     * if(existingBill.isPresent()){
-     *     Bill newBill = existingBill.get();
-     *     newBill.setId(updatedBill.getId());
-     *     newBill.setStatus(updatedBill.getStatus());
-     *     newBill.setPayee(updatedBill.getPayee());
-     *     newBill.setNickname(updatedBill.getNickname());
-     *     newBill.setCreationDate(updatedBill.getCreationDate());
-     *     newBill.setPaymentDate(updatedBill.getPaymentDate());
-     *     newBill.setRecurringDate(updatedBill.getRecurringDate());
-     *     newBill.setUpcomingPaymentDate(updatedBill.getUpcomingPaymentDate());
-     *     newBill.setPaymentAmount(updatedBill.getPaymentAmount());
-     *     newBill.setAccount(updatedBill.getAccount());
-     *     newBill.setCustomer(updatedBill.getCustomer());
-     *
-     *     billRepository.save(newBill);
-     *
-     *
-     * }else {
-     *     throw new BillByIdNotFound("Bill ID does not exist");
-     * }
-     *
-     *
-     *
-     *
-     *
-     *
-     */
-
     public Bill updateBill(Long id, Bill updatedBill) {
-        if (billRepository.containsKey(id)) {
-            updatedBill.setId(id);
-            billRepository.put(id, updatedBill);
-            return updatedBill;
-
-    public Optional<Bill> updateBill(Long id, Bill updatedBill) {
         Optional<Bill> existingBill = billRepository.findById(id);
+               if(existingBill.isPresent()){
+                   Bill newBill = existingBill.get();
+                   newBill.setId(updatedBill.getId());
+                   newBill.setStatus(updatedBill.getStatus());
+                   newBill.setPayee(updatedBill.getPayee());
+                   newBill.setNickname(updatedBill.getNickname());
+                   newBill.setCreationDate(updatedBill.getCreationDate());
+                   newBill.setPaymentDate(updatedBill.getPaymentDate());
+                   newBill.setRecurringDate(updatedBill.getRecurringDate());
+                   newBill.setUpcomingPaymentDate(updatedBill.getUpcomingPaymentDate());
+                   newBill.setPaymentAmount(updatedBill.getPaymentAmount());
+                   newBill.setAccount(updatedBill.getAccount());
+                   newBill.setCustomer(updatedBill.getCustomer());
+     billRepository.save(newBill);
+     }else {
+     throw new BillByIdNotFound("Bill ID does not exist");
+     }
+           }
+    }
+           public void deleteBill (Long id){
+                if (id == null){
+                    throw new BillsByAccountIdNotFoundException("This does not exist in bills");
+                }
+                billRepository.deleteById(id);
 
-        if (existingBill.isPresent()) {
-            updatedBill.setId(existingBill.get().getId());
-            return Optional.of(billRepository.save(updatedBill));
+            }
 
-        } else {
-            // Handle not found case, maybe throw an exception
-            return Optional.empty();
+            // Custom method to associate a bill with an account
+            public Bill createBillForAccount (Account account, Bill bill){
+
+                bill.setAccount(account);
+
+                return billRepository.save(bill);
+            }
+
         }
-    }
 
-    /**
-     * @TODO - make these changes to the deletebill
-     *
-     *
-     * public void deleteBill(Long id){
-     *     if (id == null) {
-     *         throw new BillsByIdNotFound(“This id does not exist in bills”);
-     *     }
-     *
-     *     billRepository.deleteById(id);
-     *
-     * }
-     */
-
-    public boolean deleteBill(Long id) {
-        if (billRepository.existsById(id)) {
-            billRepository.deleteById(id);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // Custom method to associate a bill with an account
-    public Bill createBillForAccount(Account account, Bill bill) {
-
-        bill.setAccount(account);
-
-        return billRepository.save(bill);
-    }
-
-}
