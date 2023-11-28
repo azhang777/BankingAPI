0bbae355b4d22b6dff23094bca206dc6eca100d7
tanzirtal <tanzirtalp@gmail.com>
2023-11-28 15:03:10 -0500

    Added scheduler to create deposit method, adjusted logic to updateDeposit method, adjusted small items in the deposit controller

--- src/main/java/com/bankapi/bankofmikaila/controller/TransactionController.java
+++ src/main/java/com/bankapi/bankofmikaila/controller/TransactionController.java
@@ -26,48 +26,5 @@ public class TransactionController {
 
     }
 
-    @GetMapping("accounts/{accountId}/deposits")
-    public ResponseEntity<?> getAllDeposits(@PathVariable Long accountId){
-
-        return transactionResponse.getAllDeposits(accountId);
-
-    }
-
-    @GetMapping("/deposits/{depositId}")
-    public ResponseEntity<?> getDeposit(@PathVariable Long depositId){
-
-        return transactionResponse.getDeposit(depositId);
-
-    }
-
-    @PostMapping("/accounts/{accountId}/deposits")
-    public ResponseEntity<?> createDeposit(@RequestBody Deposit deposit, @PathVariable Long accountId){
-
-        HttpHeaders responseHeaders = new HttpHeaders();
-        URI newDepositURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{depositId}").buildAndExpand(deposit.getId()).toUri();
-        responseHeaders.setLocation(newDepositURI);
-
-        return transactionResponse.createDeposit(deposit, accountId);
-
-    }
-
-    @PutMapping("/deposits/{depositId}")
-    public ResponseEntity<?> updateDeposit(@RequestBody Deposit deposit, @PathVariable Long depositId){
-
-        return transactionResponse.updateDeposit(deposit, depositId);
-
-    }
-
-    @DeleteMapping("/deposits/{depositId}")
-    public ResponseEntity<?> deleteDeposit(@PathVariable Long depositId){
-
-        return transactionResponse.deleteDeposit(depositId);
-
-    }
-
-
-
-
-
 
 }
